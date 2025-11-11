9744d31725eb19bd45de9d34575ea8ea878334e2
am8DAM2 <am8DAM2@informatica.iesjc>
2025-11-11 13:56:16 +0100

    Añadido el 'Flag' a signup y a Login

--- app/src/main/java/com/example/nicestart/LoginActivity.java
+++ app/src/main/java/com/example/nicestart/LoginActivity.java
@@ -26,10 +26,13 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void openMain(View view){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
     public void openSignUp(View view){
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/nicestart/SignupActivity.java
+++ app/src/main/java/com/example/nicestart/SignupActivity.java
@@ -24,6 +24,8 @@ public class  SignupActivity extends AppCompatActivity {
     }
     public void openMain(View view){
         Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
